b435965fa00ecaadd60b8aba2b54050ed538b4b3
Daniel Ortega <danort.97@gmail.com>
2025-04-29 19:21:26 -0700

    Add support for downloading individual image bands

--- bargal/images/client.py
+++ bargal/images/client.py
@@ -5,13 +5,13 @@ import requests
 from bargal.images.storage import ImageFileStore
 from bargal.models import Galaxy, GalaxyDict
 
-url_template = "https://www.legacysurvey.org/viewer/jpeg-cutout?ra={ra}&dec={dec}&size=800&layer=ls-dr10&pixscale=0.262&bands=grz"
+url_template = "https://www.legacysurvey.org/viewer/jpeg-cutout?ra={ra}&dec={dec}&size=800&layer=ls-dr10&pixscale=0.262&bands={bands}"
+supported_bands = {'g', 'r', 'z'}
 
-
-def _download_image(galaxy: Galaxy) -> bytes:
+def _download_image(galaxy: Galaxy, *, bands: str = 'grz') -> bytes:
     print(f"Downloading image for {galaxy.name} at RA: {galaxy.ra}, DEC: {galaxy.dec}")
 
-    url = url_template.format(ra=galaxy.ra, dec=galaxy.dec)
+    url = url_template.format(ra=galaxy.ra, dec=galaxy.dec, bands=bands)
 
     response = requests.get(url)
     response.raise_for_status()
@@ -27,9 +27,21 @@ class GalaxyImageClient:
         if storage_path is not None:
             self._diskcache = ImageFileStore(storage_path)
 
+    def _get_cached(self, name: str) -> bytes:
+        # Try memory cache first
+        if name in self._memcache:
+            return self._memcache[name]
+
+        # Try disk cache if available
+        if self._diskcache and self._diskcache.has_image(f"{name}.jpg"):
+            image = self._diskcache.load_image(f"{name}.jpg")
+            self._memcache[name] = image  # Cache in memory for future use
+            return image
+
     def get_image(self, galaxy: Union[Galaxy, GalaxyDict], *, save_to_disk: bool = True) -> bytes:
         """
         Gets a galaxy image from disk cache, memory cache, or downloads it.
+        The image itself will be an RGB representation of the G, R, and Z bands.
 
         Args:
             galaxy (Galaxy): The galaxy object containing its name, RA and DEC.
@@ -45,15 +57,9 @@ class GalaxyImageClient:
         """
         g = galaxy if isinstance(galaxy, Galaxy) else Galaxy.from_dict(galaxy)
 
-        # Try memory cache first
-        if g.name in self._memcache:
-            return self._memcache[g.name]
-
-        # Try disk cache if available
-        if self._diskcache and self._diskcache.has_image(f"{g.name}.jpg"):
-            image = self._diskcache.load_image(f"{g.name}.jpg")
-            self._memcache[g.name] = image  # Cache in memory for future use
-            return image
+        cached = self._get_cached(g.name)
+        if cached:
+            return cached
 
         # Download if not found in cache
         image = _download_image(g)
@@ -64,3 +70,42 @@ class GalaxyImageClient:
             self._diskcache.save_image(f"{g.name}.jpg", image)
 
         return image
+
+    def get_image_as_bands(self,
+                           galaxy: Union[Galaxy, GalaxyDict],
+                           *,
+                           save_to_disk: bool = True,
+                           bands='grz') -> dict[str, bytes]:
+        """
+        Gets a galaxy image with specific bands from disk cache, memory cache, or downloads it.
+
+        Args:
+            galaxy (Galaxy): The galaxy object containing its name, RA and DEC.
+            save_to_disk (bool): If True and storage_path is set, saves downloaded image to disk.
+                                Ignored if storage_path was not provided. Default: True.
+            bands (str): The bands to use for the image. Default: 'grz'.
+
+        Returns:
+            dict[str, bytes]: A dictionary containing the image data for each band.
+            Each key of the dictionary will correspond to a band, and the value will be the image data for that band.
+        """
+        g = galaxy if isinstance(galaxy, Galaxy) else Galaxy.from_dict(galaxy)
+        result = {}
+
+        for band in set(bands).intersection(supported_bands):
+            img_name = f"{g.name}.{band}"
+            cached = self._get_cached(img_name)
+            if cached:
+                result[band] = cached
+                continue
+
+            # Download if not found in cache
+            image = _download_image(g, bands=band)
+            self._memcache[img_name] = image
+            result[band] = image
+
+            # Save to disk if enabled
+            if self._diskcache and save_to_disk:
+                self._diskcache.save_image(f"{img_name}.jpg", image)
+
+        return result
