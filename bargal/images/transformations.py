e132e9d28d76295981bd683fffae2d893152eafa
Daniel Ortega <danort.97@gmail.com>
2025-05-07 19:03:22 -0700

    WIP: add basic image transforms

---
+++ bargal/images/transformations.py
@@ -0,0 +1,58 @@
+from astropy.visualization import PowerStretch, LogStretch, AsinhStretch, SqrtStretch, SquaredStretch
+
+
+def power(exponent: float) -> callable:
+    """
+    Apply power transformation to the image.
+
+    Args:
+        exponent (float): The exponent for the power transformation.
+
+    Returns:
+        callable: A function that applies the power transformation to an image.
+    """
+    return PowerStretch(exponent)
+    # return lambda image: np.clip(image ** exponent, 0, 1)
+
+
+def log() -> callable:
+    """
+    Apply logarithmic transformation to the image.
+
+    Returns:
+        callable: A function that applies the logarithmic transformation to an image.
+    """
+    return LogStretch()
+    # return lambda image: np.clip(np.log(1 + image), 0, 1)
+
+
+def asinh() -> callable:
+    """
+    Apply inverse hyperbolic sine transformation to the image.
+
+    Returns:
+        callable: A function that applies the inverse hyperbolic sine transformation to the image.
+    """
+    return AsinhStretch()
+    # return lambda image: np.clip(np.arcsinh(image), 0, 1)
+
+
+def squared() -> callable:
+    """
+    Apply squared transformation to the image.
+
+    Returns:
+        callable: A function that applies the squared transformation to the image.
+    """
+    return SquaredStretch()
+
+
+def sqrt() -> callable:
+    """
+    Apply square root transformation to the image.
+
+    Returns:
+        callable: A function that applies the square root transformation to the image.
+    """
+    return SqrtStretch()
+
