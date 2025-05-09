e54dc18fbe38e56ae18be928a29f78c6ac08d829
Daniel Ortega <danort.97@gmail.com>
2025-05-08 17:12:23 -0700

    Apply PR suggestion

--- bargal/images/transformations.py
+++ bargal/images/transformations.py
@@ -1,7 +1,10 @@
+from typing import Callable
+
+import numpy as np
 from astropy.visualization import PowerStretch, LogStretch, AsinhStretch, SqrtStretch, SquaredStretch
 
 
-def power_transform(exponent: float) -> callable:
+def power_transform(exponent: float) -> Callable[[np.ndarray], np.ndarray]:
     """
     Apply power transformation to the image.
 
@@ -9,50 +12,50 @@ def power_transform(exponent: float) -> callable:
         exponent (float): The exponent for the power transformation.
 
     Returns:
-        callable: A function that applies the power transformation to an image.
+        Callable[[np.ndarray], np.ndarray]: A function that applies the power transformation to an image.
     """
     return PowerStretch(exponent)
     # return lambda image: np.clip(image ** exponent, 0, 1)
 
 
-def log_transform() -> callable:
+def log_transform() -> Callable[[np.ndarray], np.ndarray]:
     """
     Apply logarithmic transformation to the image.
 
     Returns:
-        callable: A function that applies the logarithmic transformation to an image.
+        Callable[[np.ndarray], np.ndarray]: A function that applies the logarithmic transformation to an image.
     """
     return LogStretch()
     # return lambda image: np.clip(np.log(1 + image), 0, 1)
 
 
-def asinh_transform() -> callable:
+def asinh_transform() -> Callable[[np.ndarray], np.ndarray]:
     """
     Apply inverse hyperbolic sine transformation to the image.
 
     Returns:
-        callable: A function that applies the inverse hyperbolic sine transformation to the image.
+        Callable[[np.ndarray], np.ndarray]: A function that applies the inverse hyperbolic sine transformation to the image.
     """
     return AsinhStretch()
     # return lambda image: np.clip(np.arcsinh(image), 0, 1)
 
 
-def squared_transform() -> callable:
+def squared_transform() -> Callable[[np.ndarray], np.ndarray]:
     """
     Apply squared transformation to the image.
 
     Returns:
-        callable: A function that applies the squared transformation to the image.
+        Callable[[np.ndarray], np.ndarray]: A function that applies the squared transformation to the image.
     """
     return SquaredStretch()
 
 
-def sqrt_transform() -> callable:
+def sqrt_transform() -> Callable[[np.ndarray], np.ndarray]:
     """
     Apply square root transformation to the image.
 
     Returns:
-        callable: A function that applies the square root transformation to the image.
+        Callable[[np.ndarray], np.ndarray]: A function that applies the square root transformation to the image.
     """
     return SqrtStretch()
 
